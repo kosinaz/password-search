5f74861809b42a81e705e4da92daa2512b3bd952
kosinaz <kosinaz@gmail.com>
2019-03-04 19:52:31 +0100

    Extract enter

--- src/gameScene.js
+++ src/gameScene.js
@@ -74,29 +74,31 @@ const GameScene = new Phaser.Class({
       .setInteractive()
       .on('pointerup', backspace);
 
+    let enter = function () {
+      if (line < 4) {
+        line += 1;
+        for (i = 0; i < 5; i += 1) {
+          guess[i].setTint(0x006699);
+          if (guess[i].text === word[i]) {
+            guess[i].setTint(0xffff00);
+          } else if (word.includes(guess[i].text)) {
+            guess[i].setTint(0x009966);
+          }
+          guess[i] = scene.add.bitmapText(
+            384 + i * 64,
+            64 + line * 64,
+            'quadrangle',
+            '_'
+          ).setOrigin(0.5);
+        }
+        current = 0;
+      }
+    }
+
     scene.add.image(948, 524, 'atlas', 'enter_blue')
       .setOrigin(0.5)
       .setInteractive()
-      .on('pointerup', function () {
-        if (line < 4) {
-          line += 1;
-          for (i = 0; i < 5; i += 1) {
-            guess[i].setTint(0x006699);
-            if (guess[i].text === word[i]) {
-              guess[i].setTint(0xffff00);
-            } else if (word.includes(guess[i].text)) {
-              guess[i].setTint(0x009966);
-            }
-            guess[i] = scene.add.bitmapText(
-              384 + i * 64,
-              64 + line * 64, 
-              'quadrangle',
-              '_'
-            ).setOrigin(0.5);
-          }
-          current = 0;
-        }
-      });
+      .on('pointerup', enter);
     
     for (i = 0; i < 5; i += 1) {
       guess[i] = this.add.bitmapText(
@@ -118,24 +120,7 @@ const GameScene = new Phaser.Class({
         backspace();
       }
       if (event.key === 'Enter') {
-        if (line < 4) {
-          line += 1;
-          for (i = 0; i < 5; i += 1) {
-            guess[i].setTint(0x006699);
-            if (guess[i].text === word[i]) {
-              guess[i].setTint(0xffff00);
-            } else if (word.includes(guess[i].text)){
-              guess[i].setTint(0x009966);
-            }
-            guess[i] = scene.add.bitmapText(
-              384 + i * 64,
-              64 + line * 64, 
-              'quadrangle',
-              '_'
-            ).setOrigin(0.5);
-          }
-          current = 0;
-        }
+        enter();
       }
     });
   },
