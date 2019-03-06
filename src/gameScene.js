d91253465919f24eb74d72ac41eaf862c1ed5424
kosinaz <kosinaz@gmail.com>
2019-03-06 08:44:39 +0100

    Add number of missed chars

--- src/gameScene.js
+++ src/gameScene.js
@@ -40,9 +40,16 @@ const GameScene = new Phaser.Class({
     let words = this.cache.json.get('words');
     let inputField = [];
     let numberOfGuesses = 0;
-    let guessedChars = [];
+    let numberOfChars = {};
+    let guessedCharPositions = [];
     let i;
     word = words[~~(Math.random() * words.length)];
+    for (i = 0; i < word.length; i += 1) {
+      char = word.charAt(i);
+      numberOfChars[char] =
+        numberOfChars.hasOwnProperty(char) ?
+        numberOfChars[char] + 1 : 1;
+    }
     
     this.add.image(0, 0, 'atlas', 'bg').setOrigin(0);
     
@@ -51,7 +58,7 @@ const GameScene = new Phaser.Class({
 
     let addChar = function (char) {
       while (cursorPosition < word.length 
-        && guessedChars.includes(cursorPosition)) {
+        && guessedCharPositions.includes(cursorPosition)) {
           cursorPosition += 1;
       }
       if (cursorPosition < word.length) {
@@ -74,7 +81,7 @@ const GameScene = new Phaser.Class({
 
     let backspace = function () {
       while (cursorPosition > 0 
-        && guessedChars.includes(cursorPosition - 1)) {
+        && guessedCharPositions.includes(cursorPosition - 1)) {
           cursorPosition -= 1;
       }
       if (cursorPosition > 0) {
@@ -88,56 +95,75 @@ const GameScene = new Phaser.Class({
       .on('pointerup', backspace);
 
     let enter = function () {
-      while (guessedChars.includes(cursorPosition) 
+      while (guessedCharPositions.includes(cursorPosition) 
         && cursorPosition < word.length) {
           cursorPosition += 1;
       }
       if (cursorPosition < word.length) {        
         return;
       }
-      let guess = '';
-      let char;
+      let numberOfGuessedChars = {};
       if (numberOfGuesses < 4) {
         numberOfGuesses += 1;
+        
+        // Read the guess
+        let guess = '';
         for (i = 0; i < word.length; i += 1) {
-          char = inputField[i].text;
-          guess += char;
-          if (char === word.charAt(i)) {
+          guess += inputField[i].text;
+        }
+
+        // Show hits
+        for (i = 0; i < word.length; i += 1) {
+          if (guess.charAt(i) === word.charAt(i)) {
             inputField[i].setTint(0xffff00);
-            keyImages[char].setTint(0xffff00);
-            keyChars[char].setTint(0xffff00);
-            inputField[i] = scene.add.bitmapText(
-              544 - (word.length * 32) + i * 64,
-              (numberOfGuesses + 1) * 64,
-              'font',
-              char
-            ).setOrigin(0.5);
-            inputField[i].setTint(0xffff00);
-            guessedChars.push(i);
-          } else if (word.includes(char)) {
-            inputField[i].setTint(0x009966);
-            keyImages[char].setTint(0x009966);
-            keyChars[char].setTint(0x009966);
-            inputField[i] = scene.add.bitmapText(
-              544 - (word.length * 32) + i * 64,
-              (numberOfGuesses + 1) * 64,
-              'font',
-              '_'
-            ).setOrigin(0.5);
-          } else {
-            if (keys.indexOf(char) > -1) {
-              keys.splice(keys.indexOf(char), 1);
-            }
-            inputField[i].setTint(0x006699);
-            keyImages[char].setTint(0x006699);
-            keyChars[char].setTint(0x006699);
-            inputField[i] = scene.add.bitmapText(
-              544 - (word.length * 32) + i * 64,
-              (numberOfGuesses + 1) * 64,
-              'font',
-              '_'
-            ).setOrigin(0.5);
+            keyImages[guess.charAt(i)].setTint(0xffff00);
+            keyChars[guess.charAt(i)].setTint(0xffff00);
+            guessedCharPositions.push(i);
+            numberOfGuessedChars[guess.charAt(i)] =
+              numberOfGuessedChars.hasOwnProperty(guess.charAt(i)) ?
+              numberOfGuessedChars[guess.charAt(i)] + 1 : 1;
           }
+        }
+
+        // Show misses
+        for (i = 0; i < word.length; i += 1) {
+          if (word.includes(guess.charAt(i))
+            && guess.charAt(i) !== word.charAt(i)
+            && (numberOfGuessedChars[guess.charAt(i)] === undefined
+            && numberOfChars[guess.charAt(i)] > 0
+            || numberOfGuessedChars[guess.charAt(i)] 
+            < numberOfChars[guess.charAt(i)])) {
+              inputField[i].setTint(0x009966);
+              keyImages[guess.charAt(i)].setTint(0x009966);
+              keyChars[guess.charAt(i)].setTint(0x009966);
+              numberOfGuessedChars[guess.charAt(i)] =
+                numberOfGuessedChars.hasOwnProperty(guess.charAt(i)) ?
+                numberOfGuessedChars[guess.charAt(i)] + 1 : 1;
+          }
+        }
+
+        // Show fails
+        for (i = 0; i < word.length; i += 1) {
+          if (!word.includes(guess.charAt(i))) {
+            inputField[i].setTint(0x006699);
+            keyImages[guess.charAt(i)].setTint(0x006699);
+            keyChars[guess.charAt(i)].setTint(0x006699);
+            if (keys.indexOf(guess.charAt(i)) > -1) {
+              keys.splice(keys.indexOf(guess.charAt(i)), 1);
+            }
+          } else if (!inputField[i].isTinted) {
+            inputField[i].setTint(0x006699);
+          }
+        }
+
+        // Show next input field
+        for (i = 0; i < word.length; i += 1) {
+          inputField[i] = scene.add.bitmapText(
+            544 - (word.length * 32) + i * 64,
+            (numberOfGuesses + 1) * 64,
+            'font',
+            guessedCharPositions.includes(i) ? word.charAt(i) : '_'
+          ).setOrigin(0.5);
         }
         if (guess === word) {
           scene.scene.start('WinScene');
